27a92d984c93bfc2751d3e86f734d9a55c5ec52c
Pavan <loyola.pavan.74@gmail.com>
2025-06-24 20:03:57 +0530

    Added the Task details

--- python_if-else.py
+++ python_if-else.py
@@ -1,3 +1,14 @@
+print("TASK - Given an integer, , perform the following conditional actions:\n"
+      "\tIf n is odd, print Weird"
+      "\tIf n is even and in the inclusive range of 2 to 5, print Not Weird\n"
+      "\tIf n is even and in the inclusive range of 6 to 20, print Weird\n"
+      "\tIf n is even and greater than 20, print Not Weird\n")
+print("INPUT FORMAT - A single line containing a positive integer n.")
+print("CONSTRAINTS - 1 <= N <= 100")
+print("OUTPUT FORMAT - Print Weird if the number is weird. Otherwise, print Not Weird.")
+
+
+
 n = int(input())
 if n % 2 != 0:
     print("Weird")
