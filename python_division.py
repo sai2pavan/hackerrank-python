e67204a0ee659ab2fab6c5bb88e818a246739f2d
Pavan <loyola.pavan.74@gmail.com>
2025-06-26 19:27:43 +0530

    Completed #5 challenge

---
+++ python_division.py
@@ -0,0 +1,10 @@
+print("TASK - The provided code stub reads two integers a and b from STDIN"
+      "Add login to print two lines. The first one should contain the integer division a//b, The second line should contain float division a/b")
+print("EXAMPLE - a = 3, b = 5, The result of a//b = 0, the result of a/b = 0.6")
+print("INPUT FORMAT - The first line contains the first integer a, The second line contains the second integer b")
+print("OUTPUT FORMAT - Print the two lines as mentioned above")
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
