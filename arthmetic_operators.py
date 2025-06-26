f8aa8148abbc7406481a129bf096cf029d3c557f
Pavan <loyola.pavan.74@gmail.com>
2025-06-26 19:18:17 +0530

    Completed #4 challenge

---
+++ arthmetic_operators.py
@@ -0,0 +1,16 @@
+print("TASK - The provided code stub reads two integers from STDIN a and b. Add code to print three lines where:"
+      "1. The first line contains the sum of two numbers"
+      "2. The second line contains the difference of two number"
+      "3. The Third line contains the product of two numbers")
+print("EXAMPLE - a = 3, b = 5 print"
+      "\t8"
+      "\t-2"
+      "\t15")
+print("INPUT FORMAT - The first line contains the first integer a, The second line contains the second integer b")
+print("OUTPUT FORMAT - Print the three lines as explained above")
+
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
